bab356366a8ba2942b0c2e70c74bec5aa9b48cc4
yasminsd <81820504+yasminsd@users.noreply.github.com>
2022-02-14 20:56:05 +0100

    Add files via upload

---
+++ src/main/java/com/Encuentra_Tu_Hogar/Encuentra_Tu_Hogar/EncuentraTuHogarApplication.java
@@ -0,0 +1,13 @@
+package com.Encuentra_Tu_Hogar.Encuentra_Tu_Hogar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuentraTuHogarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuentraTuHogarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Encuentra_Tu_Hogar/Encuentra_Tu_Hogar/EncuentraTuHogarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Encuentra_Tu_Hogar.Encuentra_Tu_Hogar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuentraTuHogarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
